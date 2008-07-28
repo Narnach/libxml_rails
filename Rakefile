e36cbaf38ffeef00f68c70b97a93d1097aa2837c
Brandon Mitchell <brandon@systemisdown.net>
2008-07-28 15:58:18 -0700

    Initial commit.

---
+++ Rakefile
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'rake'
+
+begin
+  require 'echoe'
+  Echoe.new('libxml_rails', '0.0.1') do |p|
+    p.summary = 'A plugin substituting libxml-ruby for XmlSimple in Rails.'
+    p.url = 'http://github.com/bitbckt/libxml_rails'
+    p.author = 'Brandon Mitchell'
+    p.email = 'brandon (at) systemisdown (dot) net'
+    p.runtime_dependencies = ['activesupport >= 2.1.0', 'libxml-ruby >=0.8.3']
+  end
+rescue LoadError => boom
+  puts 'You are missing a dependency required for meta-operations on this gem.'
+  puts boom.to_s.capitalize
+end
+ 
+desc 'Install the package as a gem, without generating documentation(ri/rdoc)'
+task :install_gem_no_doc => [:clean, :package] do
+  sh "#{'sudo ' unless Hoe::WINDOZE }gem install pkg/*.gem --no-rdoc --no-ri"
+end
---
+++ lib/bitbckt/core_ext/hash/conversions.rb
@@ -0,0 +1,91 @@
+module Bitbckt #:nodoc:
+  module CoreExtensions #:nodoc:
+    module Hash #:nodoc:
+      module Conversions
+        
+        def self.included(klass)
+          klass.extend(ClassMethods)
+        end
+        
+        module ClassMethods
+          def from_xml(xml)
+            result = XML::Parser.string(xml).parse
+            if result.root.attributes['type'] == 'array'
+              { result.root.name.to_s => array_from_node(result.root) }
+            else
+              { result.root.name.to_s => xml_node_to_hash(result.root)}
+            end
+          end     
+
+          private
+            def xml_node_to_hash(node)              
+              # If we are at an XML::Node, build the Hash
+              if node.element?
+                result_hash = {}
+                if node.children?
+                  
+                  node.each_child do |child|
+                    next if child.empty?
+                  
+                    if child.attributes['type'] == 'array'
+                      result = array_from_node(child)
+                    elsif child.attributes['type'] == 'string' and child.children.empty?
+                      # NOTE: Odd issue with libxml - XML::Node#empty? returns false and
+                      #       XML::Node#children? returns true when the Node contains just
+                      #       an empty string (#children == []), but has attributes.
+                      result = ''
+                    elsif child.attributes['type'] == 'file'
+                      result = XML_PARSING['file'].call(child.content.to_s, child.attributes)
+                    elsif XML_PARSING.include? child.attributes['type'] and not child.content.to_s.chomp.blank?
+                      result = XML_PARSING[child.attributes['type']].call(child.content.to_s)
+                    else
+                      result = xml_node_to_hash(child)
+                      result['type'] = child.attributes['type'] if child.attributes['type'] and result
+                    end
+                    
+                    return result if child.name == 'text'
+                    
+                    key = child.name.gsub(/-/, '_')
+                    if result_hash[key]
+                      if result_hash[key].is_a?(Object::Array)
+                        result_hash[key] << result
+                      else
+                        result_hash[key] = [result_hash[key]] << result
+                      end
+                    else
+                      result_hash[key] = result
+                    end
+                  end
+                
+                else
+                  if node.attributes['type'] || node.attributes.length == 0 || node.attributes['nil'] == 'true'
+                    result_hash = nil
+                  else
+                    node.each_attr do |attribute|
+                      result_hash[attribute.name] = attribute.value.to_s
+                    end
+                  end
+                end
+                
+                return result_hash
+              else
+                return node.content.to_s
+              end              
+            end
+          
+            def array_from_node(node)
+              returning([]) do |collection|
+                node.each_child do |child|
+                  next if child.empty?
+                  collection << xml_node_to_hash(child)
+                end
+              end
+            end
+        end
+        
+      end
+    end
+  end
+end
+
+Hash.class_eval { include Bitbckt::CoreExtensions::Hash::Conversions }
