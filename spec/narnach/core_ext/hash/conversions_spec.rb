2ec5e4fdae1eb78daf04be395005d90bebb30a7e
Wes Oldenbeuving <narnach@gmail.com>
2008-10-02 17:38:07 +0200

    Added specs for Hash#to_xml

---
+++ spec/narnach/core_ext/hash/conversions_spec.rb
@@ -0,0 +1,23 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+
+describe Hash, '#to_xml' do
+  it "should default to not dasherize data" do
+    expected_xml = <<-XML
+<?xml version="1.0" encoding="UTF-8"?>
+<hash>
+  <post_id type="integer">1</post_id>
+</hash>
+    XML
+    {:post_id=>1}.to_xml.should == expected_xml
+  end
+  
+  it 'should allow dasherizing the data' do
+    expected_xml = <<-XML
+<?xml version="1.0" encoding="UTF-8"?>
+<hash>
+  <post-id type="integer">1</post-id>
+</hash>
+    XML
+    {:post_id=>1}.to_xml(:dasherize => true).should == expected_xml
+  end
+end
