43ec5ba9b8f2d51d8ce8465c7ffb8c786f748c6c
Wes Oldenbeuving <narnach@gmail.com>
2008-10-27 15:09:51 +0100

    Shamelessly copied a lot of the ActiveSupport internals for to_xml/builder and adapted them to work for libxml

--- lib/narnach/core_ext/hash/conversions.rb
+++ lib/narnach/core_ext/hash/conversions.rb
@@ -1,4 +1,5 @@
 require 'activesupport'
+require 'libxml'
 
 module Narnach #:nodoc:
   module CoreExtensions #:nodoc:
@@ -10,10 +11,60 @@ module Narnach #:nodoc:
           options[:dasherize]= false unless options.has_key?(:dasherize)
           super options
         end
-      end
-    end
-  end
-end
+
+        # Options supported in XMLBuilder that are/will be supported here:
+        # - :root, name of root node. Default to hash
+        # - :children, name of child nodes. Default to singular of root.
+        # - :skip_types
+        #
+        # Options supported in a different way:
+        # - :builder. Holds the active LibXML::XML::Document or LibXML::XML::Node.
+        # - :skip_instruct. Determine if the document encoding needs to be set. Probably only applies to Document.
+        # - :dasherize, dasherize the node names or keep them as-is. Defaults to false instead of true.
+        #
+        # Options known but not (yet) supported:
+        # - :indent, default to level 2
+        def to_xml_with_libxml(options = {})
+          options.reverse_merge!({ :builder => LibXML::XML::Document.new, :root => "hash", :dahserize => false })
+          dasherize = options[:dasherize]
+          doc = options[:builder]
+          root = dasherize ? options[:root].to_s.dasherize : options[:root].to_s
+          unless options.delete(:skip_instruct)
+            doc.encoding = 'UTF-8'
+            doc.root = LibXML::XML::Node.new(root)
+            doc = doc.root
+          end
+          self.each do |key, value|
+            case value
+            when ::Hash
+            # when ::Array
+            # when ::Method, ::Proc
+            else
+              if value.respond_to?(:to_xml_with_libxml)
+                value.to_xml_with_libxml(options.merge({ :root => key, :skip_instruct => true }))
+              else
+                type_name = ActiveSupport::CoreExtensions::Hash::Conversions::XML_TYPE_NAMES[value.class.name]
+                key = dasherize ? key.to_s.dasherize : key.to_s
+                attributes = options[:skip_types] || value.nil? || type_name.nil? ? { } : { :type => type_name }
+                if value.nil?
+                  attributes[:nil] = true
+                end
+
+                child = LibXML::XML::Node.new(key, ActiveSupport::CoreExtensions::Hash::Conversions::XML_FORMATTING[type_name] ? ActiveSupport::CoreExtensions::Hash::Conversions::XML_FORMATTING[type_name].call(value) : value)
+                attributes.stringify_keys.each do |akey, avalue|
+                  child[akey] = avalue
+                end
+                doc << child
+              end # if
+            end # case
+          end # each
+          yield options[:builder] if block_given?
+          return options[:builder]
+        end # to_xml_with_libxml
+      end # module Conversions
+    end # module Hash
+  end # module CoreExtensions
+end # module Narnach
 
 class Hash
   include Narnach::CoreExtensions::Hash::Conversions
--- spec/narnach/core_ext/hash/conversions_spec.rb
+++ spec/narnach/core_ext/hash/conversions_spec.rb
@@ -1,5 +1,5 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
-require 'libxml_rails'
+require 'narnach/core_ext/hash/conversions'
 
 describe Hash, '#to_xml' do
   it "should default to not dasherize data" do
@@ -20,5 +20,14 @@ describe Hash, '#to_xml' do
 </hash>
     XML
     {:post_id=>1}.to_xml(:dasherize => true).should == expected_xml
+  end
+end
+
+describe Hash, "#to_xml_with_libxml" do
+  it "should convert a Hash with one element" do
+    hsh = {:one => 1}
+    libxml_xml = hsh.to_xml_with_libxml.to_s
+    builder_xml = hsh.to_xml
+    libxml_xml.should == builder_xml
   end
 end
